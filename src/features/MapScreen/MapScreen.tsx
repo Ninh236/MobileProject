3932ea9981b274cffd1f44980c10f5957aee7549
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2024-04-13 00:51:00 +0700

    Add map view

--- src/features/MapScreen/MapScreen.tsx
+++ src/features/MapScreen/MapScreen.tsx
@@ -1,10 +1,32 @@
-import { Text } from 'native-base'
-import React from 'react'
+import { Box, Text } from 'native-base'
+import React, { useEffect, useState } from 'react'
+import * as Location from 'expo-location'
+
+import { styles } from './styles'
+import MapView from 'react-native-maps'
 
 export default function MapScreen() {
+  let [location, setLocation] = useState<Location.LocationObject | null>(null)
+
+  useEffect(() => {
+    void (async function () {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied')
+        return
+      }
+
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+      console.log(location)
+    })()
+  }, [])
+
   return (
     <React.Fragment>
-      <Text>Map Screen</Text>
+      <Box style={styles.viewStyles}>
+        <MapView style={styles.mapStyles} />
+      </Box>
     </React.Fragment>
   )
 }
--- src/features/MapScreen/styles.ts
+++ src/features/MapScreen/styles.ts
@@ -1,3 +1,12 @@
 import { StyleSheet } from 'react-native'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
 
-export const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+  viewStyles: {
+    paddingTop: getStatusBarHeight(),
+  },
+  mapStyles: {
+    width: '100%',
+    height: '100%',
+  },
+})
